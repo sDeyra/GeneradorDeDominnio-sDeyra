7ddd650c784326c0a658758971fff85a92d1d671
sDeyra <sdeymt@gmail.com>
2025-07-08 22:23:59 +0000

    Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+   let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for(let i = 0; i<pronoun.length; i++){
+    for (let o = 0; o < adj.length; o++) {
+      for (let p = 0; p < noun.length; p++) {
+        console.log(pronoun[i] +adj[o] +noun[p] +".com")
+        
+      }
+    }
+    
+  }
+
 };
